510fc1db4df02b168de6f995d4526b0ecda8136a
HenryChinask1 <jeffbaier72@yahoo.com>
2024-11-08 16:11:53 -0500

    template work

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,12 @@
 from flask import Flask, redirect, render_template, url_for, request
 
 app = Flask(__name__, static_url_path='/static')
-app.config['DEBUG'] = True
+#app.config['DEBUG'] = True
 
 @app.route('/')
 def home():
     return render_template('main_page.html')
 
+@app.route('/rules')
+def rules():
+    return render_template('rules_page.html')
